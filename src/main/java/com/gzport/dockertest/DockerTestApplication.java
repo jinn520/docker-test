08350518c16e3c588eab64266fe7dd63f0761cae
jinn520 <1204443635@qq.com>
2019-11-12 09:44:33 +0800

    initial

---
+++ src/main/java/com/gzport/dockertest/DockerTestApplication.java
@@ -0,0 +1,21 @@
+package com.gzport.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerTestApplication {
+
+    @GetMapping(value = "/test")
+    public Object test() {
+        return "Hello SpringBoot with Docker!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTestApplication.class, args);
+    }
+
+}
